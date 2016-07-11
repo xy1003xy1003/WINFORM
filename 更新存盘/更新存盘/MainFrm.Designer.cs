fa13dee7caf3f9a7ec5a15cf78c69bf3b96953c3
xy1003xy1003 <838087568@qq.com>
2016-07-11 10:27:16 +0800

    add

---
+++ 更新存盘/更新存盘/MainFrm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace 更新存盘
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSyncOut = new System.Windows.Forms.Button();
+            this.btnSyncIn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSyncOut
+            // 
+            this.btnSyncOut.AccessibleDescription = "";
+            this.btnSyncOut.Enabled = false;
+            this.btnSyncOut.Location = new System.Drawing.Point(22, 16);
+            this.btnSyncOut.Name = "btnSyncOut";
+            this.btnSyncOut.Size = new System.Drawing.Size(102, 52);
+            this.btnSyncOut.TabIndex = 1;
+            this.btnSyncOut.Tag = "";
+            this.btnSyncOut.Text = "同步U盘";
+            this.btnSyncOut.UseVisualStyleBackColor = true;
+            this.btnSyncOut.Click += new System.EventHandler(this.btnSyncOut_Click);
+            // 
+            // btnSyncIn
+            // 
+            this.btnSyncIn.Location = new System.Drawing.Point(158, 16);
+            this.btnSyncIn.Name = "btnSyncIn";
+            this.btnSyncIn.Size = new System.Drawing.Size(102, 52);
+            this.btnSyncIn.TabIndex = 2;
+            this.btnSyncIn.Text = "同步电脑";
+            this.btnSyncIn.UseVisualStyleBackColor = true;
+            this.btnSyncIn.Click += new System.EventHandler(this.btnSyncIn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 88);
+            this.Controls.Add(this.btnSyncIn);
+            this.Controls.Add(this.btnSyncOut);
+            this.MaximumSize = new System.Drawing.Size(297, 126);
+            this.MinimumSize = new System.Drawing.Size(297, 126);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "存盘更新";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSyncOut;
+        private System.Windows.Forms.Button btnSyncIn;
+    }
+}
+
---
+++ 更新存盘/更新存盘/MainFrm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 更新存盘
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// PCPath
+        /// </summary>
+        private string _pcPath = string.Empty;
+        /// <summary>
+        /// U盘Path
+        /// </summary>
+        private string _uPath = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 同步U盘
+        /// 以U盘中文件为基准，同步到电脑中
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSyncOut_Click(object sender, EventArgs e)
+        {
+            string[] files = Directory.GetFiles(_uPath, "*.*", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                string newFilePath = file.Replace(_uPath, _pcPath);
+                string newPath = Path.GetDirectoryName(newFilePath);
+                if (!Directory.Exists(newPath))
+                {
+                    Directory.CreateDirectory(newPath);
+                }
+                File.Copy(file, newFilePath, true);
+            }
+            MessageBox.Show("同步完成");
+        }
+        /// <summary>
+        /// 同步电脑
+        /// 以电脑中文件为基准，同步到U盘中
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSyncIn_Click(object sender, EventArgs e)
+        {
+            string[] files = Directory.GetFiles(_pcPath, "*.*", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                string newFilePath = file.Replace(_pcPath, _uPath);
+                string newPath = Path.GetDirectoryName(newFilePath);
+                if (!Directory.Exists(newPath))
+                {
+                    Directory.CreateDirectory(newPath);
+                }
+                File.Copy(file, newFilePath, true);
+            }
+            MessageBox.Show("同步完成");
+        }
+
+        /// <summary>
+        /// Load检查
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.VolumeLabel.ToUpper().Equals("STYLE"))
+                {//存在Style盘
+                    _uPath = Path.Combine(drive.Name, "存盘");
+
+                    _pcPath = Path.Combine("D:\\works", "存盘");
+                }
+            }
+
+            if (string.IsNullOrEmpty(_pcPath) || string.IsNullOrEmpty(_uPath))
+            {
+                MessageBox.Show("尚未插入U盘");
+                this.Close();
+            }
+        }
+    }
+}
